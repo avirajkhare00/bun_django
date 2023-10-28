c3830483f90da319ae5a89ee2ba06a6262f6f7fd
avirajkhare00 <avirajkhare00@gmail.com>
2023-10-28 11:20:18 +0530

    [Aviraj] Error in imports

--- django_admin_commands.sh
+++ django_admin_commands.sh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 
+set -ex
 PYTHON_LOCATION=$(which python3.11)
 DJANGO_ADMIN_LOCATION=$(pwd)/django_package/bin
 
-$PYTHON_LOCATION $DJANGO_ADMIN_LOCATION/django-admin dbshell
+# $PYTHON_LOCATION $(pwd)/django_settings_pkg/settings/settings.py
+
+DJANGO_SETTINGS_MODULE=$(pwd)/django_settings_pkg.settings.settings $PYTHON_LOCATION -m django_settings_pkg.settings $DJANGO_ADMIN_LOCATION/django-admin dbshell
+# $PYTHON_LOCATION -m $(pwd)/django_settings_pkg.settings $DJANGO_ADMIN_LOCATION/django-admin dbshell
+
+#  -> !/opt/homebrew/opt/python@3.11/bin/python3.11 --< this will autogenerate, once we scaffold
+# -*- coding: utf-8 -*-
+# import re
+# import sys
