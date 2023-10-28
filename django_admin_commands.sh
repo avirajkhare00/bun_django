7d716e6d21541ae8e35582b61cef036be561d2c4
avirajkhare00 <avirajkhare00@gmail.com>
2023-10-28 12:12:01 +0530

    [Aviraj] Debug and some progress

--- django_admin_commands.sh
+++ django_admin_commands.sh
@@ -2,11 +2,12 @@
 
 set -ex
 PYTHON_LOCATION=$(which python3.11)
+PYTHON_PATH=$(which python3.11):$(pwd)/django_settings_pkg
 DJANGO_ADMIN_LOCATION=$(pwd)/django_package/bin
 
 # $PYTHON_LOCATION $(pwd)/django_settings_pkg/settings/settings.py
 
-DJANGO_SETTINGS_MODULE=$(pwd)/django_settings_pkg.settings.settings $PYTHON_LOCATION -m django_settings_pkg.settings $DJANGO_ADMIN_LOCATION/django-admin dbshell
+$PYTHON_LOCATION $DJANGO_ADMIN_LOCATION/django-admin dbshell
 # $PYTHON_LOCATION -m $(pwd)/django_settings_pkg.settings $DJANGO_ADMIN_LOCATION/django-admin dbshell
 
 #  -> !/opt/homebrew/opt/python@3.11/bin/python3.11 --< this will autogenerate, once we scaffold
